f620d00747f37b08ad610cd325902542e21c2645
bunnelldp-sketch <bunnellduncan2@gmail.com>
2025-09-25 14:24:20 -0600

    Update XmlReaderTest.java

--- src/test/java/com/cburch/logisim/file/XmlReaderTest.java
+++ src/test/java/com/cburch/logisim/file/XmlReaderTest.java
@@ -14,6 +14,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+/**
+Hello world
+*/
+
 
 public class XmlReaderTest {
 
